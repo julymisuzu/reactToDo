92bc66d634cca3a1eee49629b18b67ff619cca59
Juliana Flor <juliana.santos@osf-global.com>
2020-01-22 11:13:22 -0300

    Trying to fix firebase in production

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -5,7 +5,7 @@ try {
   var config = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
+    databaseURL:process.env.DATABASE_URL,
     storageBucket: process.env.STORAGE_BUCKET
   };
 
